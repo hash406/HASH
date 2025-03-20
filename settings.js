10afec018401cfa603004dbdeec671f1be054c39
hash406 <hashindukavinda@gmail.com>
2025-03-20 12:55:20 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
-  packname: 'Knight',
+  packname: 'Dark',
   author: 'Bot',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '917023951514', //Your number
+  botName: "Dark Bot",
+  botOwner: 'Hash', // Your name
+  ownerNumber: '94757653361', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
